4cdc47dd5013b834efd06497851e4fa28795ed0e
Anderson Dantas <a.kleber.d@gmail.com>
2019-03-08 12:30:03 -0300

    Changes to template

--- contest-template/a/code.cpp
+++ contest-template/a/code.cpp
@@ -1,24 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define trace1(a) cout << a << '\n';
-#define trace2(a, b) cout << a << ' ' << b << '\n';
-#define trace3(a, b, c) cout << a << ' ' << b << ' ' << c << '\n';
-#define trace4(a, b, c, d) cout << a << ' ' << b << ' ' << c << ' ' << d << '\n';
-#define trace5(a, b, c, d, e) cout << a << ' ' << b << ' ' << c << ' ' << d << ' ' << e << '\n';
-#define trace6(a, b, c, d, e, f) cout << a << ' ' << b << ' ' << c << ' ' << d << ' ' << e << ' ' << f << '\n';
-#define printArr(harry, tam) range(tam) cout << harry[i] << " \n"[i == tam -1];
 #define range(n) for(int i = 0; i < n; i++)
-#define maxn 2000000
+#define printArr(harry, tam) range(tam) cout << harry[i] << " \n"[i == tam -1];
+#define maxn 50010
 #define mod 1000000007
 #define md(x) (x) % mod;
-#define sc scanf
-#define pr printf
 #define fi first
 #define se second
 #define mp make_pair
 #define pb push_back
-#define ps push
-#define prt(x) cout << (#x) << " is " << (x) << endl
 #define EPS 1e-9
 #define INF 1000000000
 #define INFd 1e9
@@ -29,6 +19,17 @@ typedef vector<int> vi;
 typedef vector<ii> vii;
 typedef vector<ll> vll;
 typedef vector<double> vd;
+
+template<typename T>
+void trace(T a){
+	cout << a << '\n';
+}
+
+template<typename T, typename... Args>
+void trace(T a, Args... args){
+	cout << a << ' ';
+	trace(args...);
+}
 
 int main(){
 	int n;
