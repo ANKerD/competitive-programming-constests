8c01bfb3116a8798abb2af487781dba86b094fa6
Anderson Kleber Dantas <a.kleber.d@gmail.com>
2019-04-27 18:10:35 -0300

    Implemented A - Breaking String interactive

--- aaa-2019.1/lista-4/a/code.cpp
+++ aaa-2019.1/lista-4/a/code.cpp
@@ -19,14 +19,21 @@ void trace(T a, Args ...args){ cout << a << ' '; trace(args...);}
 
 int n, m, a[maxn];
 int dp[maxn][maxn];
+int op[maxn][maxn];
 
 int solve(int i, int j){
-	if(i+1>=j) return 0;
+	if(i+1>=j){
+		op[i][j] = i;
+		return 0;
+	}
 	if(dp[i][j] != -1) return dp[i][j];
 
 	int ans = 1e8;
 	for(int k = i+1; k < j; k++)
-		ans = min(ans, solve(i, k) + solve(k, j));
+		if(ans > solve(i, k) + solve(k, j)){
+			ans = solve(i, k) + solve(k, j); 
+			op[i][j] = k;
+		}
 	
 	int d = a[j]-a[i];
 	return dp[i][j] = ans+d;
@@ -38,8 +45,35 @@ int main(){
 		a[m+1] = n;
 		for(int i = 1; i <= m; i++)
 			cin >> a[i];
+		
 		memset(dp, -1, sizeof dp);
-		cout << solve(0, m+1) << '\n';
+		for(int i = 1; i <= m+1; i++)
+			for(int j = 0; i+j <= m+1; j++){
+				if(i == 1){
+					dp[j][j+i] = 0;
+					// op[j][j+i] = i;
+					continue;
+				}
+
+				int ans = 1e9;
+				for(int k = j+1; k < i+j; k++){
+					if(ans > dp[j][k]+dp[k][j+i]){
+						ans = dp[j][k]+dp[k][j+i];
+						op[j][j+i] = k;
+
+					}
+				}
+				dp[j][j+i] = ans + a[j+i] - a[j];
+			}
+
+			
+		// cout << solve(0,m+1) << '\n';
+		// cout << solve(0,m+1) << '\n';
+		for(int i = 0; i <= m+1; i++){
+			for(int j = 0; j <= m+1; j++)
+				printf("%d %d\t", dp[i][j], op[i][j]);
+			cout << '\n';
+		}
 	}
  	return 0;
 }
