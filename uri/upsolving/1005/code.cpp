a5414c4aea74881680dcf842c29488703ceac604
Charles Bezerra de Oliveira Junior <charlesbezerra5@gmail.com>
2021-10-31 20:03:03 -0300

    1005

---
+++ uri/upsolving/1005/code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+ 
+int main() {
+	double A, B;
+	
+	cin>>A;
+	cin>>B;
+	
+	double MEDIA = ((A*3.5) + (B*7.5))/11;
+
+	cout<<"MEDIA = "<<fixed<<setprecision(5)<<MEDIA<<endl;	
+	return 0;
+}
