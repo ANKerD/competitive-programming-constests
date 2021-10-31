d2dd24372018a48d1568bcb1c92324fea0d6f81b
Charles Bezerra de Oliveira Junior <charlesbezerra5@gmail.com>
2021-10-31 19:59:50 -0300

    1004

---
+++ uri/upsolving/1004/code.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+ 
+    int A, B;
+    
+ 	
+ 	cin>>A;
+ 	cin>>B;
+ 	
+ 	int PROD = A * B; 	
+ 	
+ 	cout<<"PROD = "<< PROD<<endl;
+ 	
+    return 0;
+}
